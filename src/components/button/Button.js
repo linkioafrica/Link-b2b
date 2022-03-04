e61cf28e81828fb6d381c9c668180d97f89f155b
Tansi Jones <tansijones@outlook.com>
2022-03-04 14:41:29 +0200

    Update Button design prop structure

---
+++ src/components/button/Button.js
@@ -0,0 +1,12 @@
+export const Button = ({ icon, bgColor, ringColor, text, textSize }) => {
+  return (
+    <button
+      className={`flex items-center justify-center text-white font-normal ${textSize} rounded-lg ${bgColor} px-10 py-3 active:ring ${ringColor} transition duration-200 ease-out cursor-pointer ${
+        icon && "space-x-2"
+      }`}
+    >
+      {icon && <span className="text-xl ">{icon}</span>}
+      <span>{text}</span>
+    </button>
+  );
+};
