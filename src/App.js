faf1900b61e3c1428fc5a09fb84a9b03d76e7986
Tansi Jones <tansijones@outlook.com>
2022-03-04 15:23:55 +0200

    No significant change, changing branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Button } from "./components/button/Button";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button
+        bgColor="bg-blue-500"
+        ringColor="ring-blue-200"
+        text="Hello"
+        textSize="text-lg"
+      />
+    </div>
+  );
 };
 
 export default App;
