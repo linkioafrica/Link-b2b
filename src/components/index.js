10cc0e3ede28ddc1835c9be26f896805b970fdea
Unique-coder <Tom-Lesh@outlook.com>
2022-03-05 14:29:01 +0100

    Request component folder created

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as Button } from "./button/Button";
 export { default as Navbar } from "./navbar/Navbar";
+export { default as Button } from "./button/Button";
+export { default as Request } from "./request/Request";
 export { default as Balance } from "./balance/Balance";
 export { default as Converter } from "./converter/Converter";
